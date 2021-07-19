40e4241ee5e2957976cd2c10b1875ade4610a480
DanielM-Qva <jmastrapa96@gmail.com>
2021-07-19 17:43:47 -0400

    Redirec Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
 import { AuhtCantActive } from './././Services/CanActive';
 import { HomComponent } from './public/hom/hom.component';
-import {LoginComponent} from './Modules/auht/pages/login/login.component';
+import { LoginComponent } from './Modules/auht/pages/login/login.component';
 
 
 
 
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./Modules/auht/auht.module').then(m => m.AuhtModule) },
-   {path:"mconocimiento",canActivate:[AuhtCantActive],loadChildren: ()=> import('./Modules/m-conocimiento/m-conocimiento.module').then(m =>m.MConocimientoModule)},
-  { path: 'admin', canActivate: [AuhtCantActive], loadChildren: () => import('./Modules/Admin/admin.module').then(m => m.AdminModule) } ,
-   {path: 'products' ,canActivate:[AuhtCantActive] ,  loadChildren: () => import('./Modules/m-client/m-client.module').then(m => m.MClientModule)},
-  {path:'home'  ,component:LoginComponent},
-   {path:'' , component:LoginComponent},
+  { path: "mconocimiento", canActivate: [AuhtCantActive], loadChildren: () => import('./Modules/m-conocimiento/m-conocimiento.module').then(m => m.MConocimientoModule) },
+  { path: 'admin', canActivate: [AuhtCantActive], loadChildren: () => import('./Modules/Admin/admin.module').then(m => m.AdminModule) },
+  { path: 'products', canActivate: [AuhtCantActive], loadChildren: () => import('./Modules/m-client/m-client.module').then(m => m.MClientModule) },
+  { path: 'home', component: HomComponent },
+  { path: '', component: LoginComponent },
   { path: 'infraes', loadChildren: () => import('./Modules/m-infraestructura/m-infraestructura.module').then(m => m.MInfraestructuraModule) },
-    {path:"**" , canActivate:[AuhtCantActive] , component:HomComponent}
+  { path: "**", canActivate: [AuhtCantActive], component: HomComponent }
 
 ];
 
